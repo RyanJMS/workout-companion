f84b9040a0fbe803a6d6881e85b6b301f7119d65
Ryan <jukkasc2@gmail.com>
2022-02-28 08:46:26 -0500

    Fetch Structure, and Live Search functionality && Structure of App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [exercises, setExercises] = useState([]);
+  const [search, setSearch] = useState("");
+  const [timeoutId, updateTimeoutId] = useState();
+
+  const fetchData = (searchString) => {
+    fetch(
+      `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${searchString}`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+          "x-rapidapi-key":
+            "92fde71775msh41f0c9b7fb596d4p136847jsne0f47f0738ab",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setExercises(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const onTextChange = (event) => {
+    clearTimeout(timeoutId);
+    setSearch(event.target.value);
+    const timeout = setTimeout(() => fetchData(event.target.value), 500);
+    updateTimeoutId(timeout);
+  };
+
   return <div>App</div>;
 };
 
