6ba7c1598d2e497937e99e3a3761fc467dc70819
Ryan <jukkasc2@gmail.com>
2022-03-02 00:48:37 -0500

    attempt #8

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import ExerciseList from "./components/ExerciseList";
 import Home from "./components/Home";
